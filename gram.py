8f39700b553ef1023827bad23c067c52f5c8c3c8
Achykez <chyke.ghoul@gmail.com>
2023-03-27 16:42:39 +0100

    second commit

---
+++ gram.py
@@ -0,0 +1,86 @@
+import tkinter
+import customtkinter
+import os
+import requests
+import json
+
+
+# Function to get download folder based on operating system
+def get_download_folder():
+    if os.name == "nt":
+        # Windows
+        return os.path.join(os.environ["USERPROFILE"], "Downloads")
+    elif os.name == "posix":
+        # Linux or macOS
+        return os.path.join(os.path.expanduser("~"), "Downloads")
+    else:
+        # Unsupported OS
+        raise OSError("Unsupported operating system")
+
+
+# Function to start downloading Instagram video
+def beginDownload():
+    try:
+        instaLink = link.get()
+        resp = requests.get(instaLink)
+        shortcode = resp.url.split("/")[-2]
+        instaApiUrl = f"https://www.instagram.com/p/{shortcode}/?__a=1"
+        instaApiResp = requests.get(instaApiUrl)
+        instaData = json.loads(instaApiResp.text)
+        videoUrl = instaData["graphql"]["shortcode_media"]["video_url"]
+        resp = requests.get(videoUrl, stream=True)
+        total_size = int(resp.headers.get('content-length', 0))
+        block_size = 1024
+        download_folder = get_download_folder()
+        filePath = os.path.join(download_folder, f"{shortcode}.mp4")
+        with open(filePath, 'wb') as f:
+            for data in resp.iter_content(block_size):
+                if data:
+                    f.write(data)
+                    bytes_downloaded = len(data)
+                    percentage_done = bytes_downloaded / total_size * 100
+                    per = str(int(percentage_done))
+                    pPercentage.configure(text=per + "%")
+                    pPercentage.update()
+                    progressBar.set(float(percentage_done) / 100)
+        finishLabel.configure(text="Downloaded", text_color="green")
+    except:
+        finishLabel.configure(text="Couldn't download", text_color="red")
+
+
+# System settings
+customtkinter.set_appearance_mode('System')
+customtkinter.set_default_color_theme("green")
+
+# Our Application frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Instagram downloader")
+
+# Adding UI elements
+title = customtkinter.CTkLabel(app, text="Insert Instagram link")
+title.pack(padx=10, pady=10)
+
+# Link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link.pack()
+
+# Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+# Progress Bar
+pPercentage = customtkinter.CTkLabel(app, text="0 %")
+pPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
+
+# Download button
+download = customtkinter.CTkButton(app, text="Download", command=beginDownload)
+download.pack(padx=10, pady=10)
+
+# app runner
+app.mainloop()
--- main.py
+++ main.py
@@ -1,23 +1,67 @@
 import tkinter
 import customtkinter
+import os
 from pytube import YouTube
 
+
+# System settings
+customtkinter.set_appearance_mode('System')
+customtkinter.set_default_color_theme("green")
+
+def get_download_folder():
+    if os.name == "nt":
+        # Windows
+        return os.path.join(os.environ["USERPROFILE"], "Downloads")
+    elif os.name == "posix":
+        # Linux or macOS
+        return os.path.join(os.path.expanduser("~"), "Downloads")
+    else:
+        # Unsupported OS
+        raise OSError("Unsupported operating system")
 
 def beginDownload():
     try:
         ytLink = link.get()
-        ytObject = YouTube(ytLink)
+        ytObject = YouTube(ytLink, on_progress_callback=in_progress)
         video = ytObject.streams.get_highest_resolution()
-
-
-        title.configure(text=ytObject.title, text_color="white")
+        title.configure(text=ytObject.title, text_color="blue")
         finishLabel.configure(text="")
-        video.download("C:/Users/USER\Desktop/Achike's Work/pylab")
-        finishLabel.configure(text="Downloaded")
+        download_folder = get_download_folder()
+        file_path = os.path.join(download_folder, ytObject.title + ".mp4")
+        if os.path.isfile(file_path):
+            # Prompt the user to choose whether to overwrite or rename the file
+            choice = customtkinter.CTkMessageBox.askquestion(
+                "File already exists",
+                "A file with this name already exists in your downloads folder. Do you want to overwrite it?",
+                icon="warning"
+            )
+            if choice == "no":
+                # Prompt the user to enter a new file name
+                file_name = customtkinter.CTkSimpleDialog.askstring(
+                    "File name",
+                    "Please enter a new name for the file:"
+                )
+                if file_name:
+                    file_path = os.path.join(download_folder, file_name + ".mp4")
+                else:
+                    # If the user didn't enter a new file name, stop the download
+                    return
+        video.download(file_path)
+        finishLabel.configure(text="Downloaded", text_color="green")
     except:
         finishLabel.configure(text="Couldn't download", text_color="red")
 
 
+
+def in_progress(stream, chunk, bytes_remaining):
+    total_size= stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_done = bytes_downloaded/ total_size * 100
+    per = str(int(percentage_done)) 
+    pPercentage.configure(text=per + "%")
+    pPercentage.update()
+
+    progressBar.set(float(percentage_done) / 100)
 # System settings
 customtkinter.set_appearance_mode('System')
 customtkinter.set_default_color_theme("green")
@@ -38,6 +82,15 @@ link.pack()
 # Finished Downloading
 finishLabel = customtkinter.CTkLabel(app, text="")
 finishLabel.pack()
+
+#Progress Bar
+
+pPercentage =customtkinter.CTkLabel(app, text="0 %")
+pPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
 
 # Download button
 download = customtkinter.CTkButton(app, text="Download", command=beginDownload)
